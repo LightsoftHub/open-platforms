cce31819f383bd7e11d4084d28c0855de0087f2b
minh.vu <minhvu.box@gmail.com>
2025-05-22 11:15:14 +0700

    add Grab Auth

---
+++ src/Grab.Auth/GrabAuthClient.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Light.Grab.Auth
+{
+    public class GrabAuthClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public GrabAuthClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<TokenResponse> GetTokenAsync(string clientId, string clientSecret, string scope)
+        {
+            var path = "grabid/v1/oauth2/token";
+
+            var request = new
+            {
+                client_id = clientId,
+                client_secret = clientSecret,
+                grant_type = "client_credentials",
+                scope
+            };
+
+            var response = await _httpClient.PostAsJsonAsync(path, request);
+
+            return await response.Content.ReadFromJsonAsync<TokenResponse>();
+        }
+    }
+}
